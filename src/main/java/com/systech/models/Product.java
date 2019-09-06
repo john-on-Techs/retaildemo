5065f560c419aacd009c15f14f31cfe538e4ea3f
FrancisSakwa89 <sakwafrancis89@gmail.com>
2019-09-06 16:15:57 +0300

    acted on products

--- src/main/java/com/systech/models/Product.java
+++ src/main/java/com/systech/models/Product.java
@@ -4,4 +4,15 @@ package com.systech.models;
  * Created by Harriet on 9/5/2019.
  */
 public class Product {
+    public int id;
+    public String name;
+
+    public Product() {
+    }
+
+    public Product(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 }
+
