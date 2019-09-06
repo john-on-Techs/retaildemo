647c9db0b11adbd35fdd576e06d734dd0c65988f
john-on-Techs <gavanayeye@gmail.com>
2019-09-06 16:17:56 +0300

    acted on the same product class

--- src/main/java/com/systech/models/Product.java
+++ src/main/java/com/systech/models/Product.java
@@ -4,8 +4,10 @@ package com.systech.models;
  * Created by Harriet on 9/5/2019.
  */
 public class Product {
+
     public int id;
     public String name;
+    private String productDescription;
 
     public Product() {
     }
@@ -13,6 +15,30 @@ public class Product {
     public Product(int id, String name) {
         this.id = id;
         this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getProductDescription() {
+        return productDescription;
+    }
+
+    public void setProductDescription(String productDescription) {
+        this.productDescription = productDescription;
     }
 }
 
